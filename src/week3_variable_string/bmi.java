fe863357141093096fc58752db9d4569b4d6afb2
[Kendall] <huangzi435@gmail.com>
2023-09-09 23:17:40 +0800

    bmi

---
+++ src/week3_variable_string/bmi.java
@@ -0,0 +1,16 @@
+package week3_variable_string;
+
+public class bmi {
+
+    public static void main(String[] args){
+
+        double height = 1.56;
+        double weight = 58.2;
+        double bmi = weight / Math.pow(height, 2);
+
+        System.out.println(bmi);
+    }
+}
+
+
+// BMI計算
