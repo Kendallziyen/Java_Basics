18ccacd69f3ab4ffcc4c99255429d2469b2ea77f
[Kendall] <huangzi435@gmail.com>
2023-09-12 23:33:29 +0800

    week 4 updated

---
+++ src/week4_type_conversion_operators/default_value.java
@@ -0,0 +1,16 @@
+package week4_type_conversion_operators;
+
+public class default_value {
+
+    public static void main(String[] args){
+
+        long l = 2147483648L;
+        float f = 1.23F;
+
+        System.out.println(l);
+        System.out.println(f);
+    }
+}
+
+
+//告訴編譯器⽤long, float配置長度
---
+++ src/week4_type_conversion_operators/wrapper_types.java
@@ -0,0 +1,14 @@
+package week4_type_conversion_operators;
+
+public class wrapper_types {
+
+    public static void main(String[] args){
+
+        Double d = new Double(3.14);
+
+        System.out.println(d.SIZE);
+    }
+}
+
+
+//將基本資料型態包裝成物件
