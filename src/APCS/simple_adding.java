6a396f946dd84785d3337824475a3a2f0eb48c70
kendallziyen <huangzi435@gmail.com>
2023-12-31 23:56:37 +0800

    APCS

---
+++ src/APCS/simple_adding.java
@@ -0,0 +1,17 @@
+package APCS;
+
+import java.util.Scanner;
+public class simple_adding{
+
+    public static void main(String[] args){
+
+        Scanner std = new Scanner(System.in);
+
+        while (std.hasNextInt()){
+
+            int n1 = std.nextInt();
+            int n2 = std.nextInt();
+            System.out.println(n1+n2);
+        }
+    }
+}
