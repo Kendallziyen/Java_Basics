ae6c625566d09690f8abdebe1fe63e74efb9025b
kendallziyen <huangzi435@gmail.com>
2023-12-31 22:12:21 +0800

    APCS

---
+++ src/APCS/hello.java
@@ -0,0 +1,13 @@
+package APCS;
+
+import java.util.Scanner;
+
+
+public class hello {
+    public static void main(String[] args){
+
+        Scanner myObj = new Scanner(System.in);
+        String std = myObj.nextLine();
+        System.out.println("hello, " + std);
+    }
+}
