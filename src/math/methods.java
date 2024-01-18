0c59c51e53298c3941988a28b9d44e46139c7526
kendallziyen <huangzi435@gmail.com>
2024-01-18 22:47:09 +0800

    math

---
+++ src/math/methods.java
@@ -0,0 +1,18 @@
+package math;
+
+public class methods {
+
+    public static void main(String[] args){
+
+        System.out.println(Math.max(5,10));
+        System.out.println(Math.min(5,10));
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.abs(-5));   //returns the absolute (positive) value of x
+        System.out.println(Math.random());  //generates a random number between 0 and 1
+
+
+        //generates a random number between 0 and 100
+        int randomNum = (int)(Math.random()*101);
+        System.out.println(randomNum);
+    }
+}
