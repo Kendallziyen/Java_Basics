34b857c246d3774f778cf6d326f1c3ee90d4cbb4
kendallziyen <huangzi435@gmail.com>
2024-01-20 23:10:06 +0800

    func_methods

---
+++ src/func_methods/recursion_halting.java
@@ -0,0 +1,21 @@
+package func_methods;
+
+public class recursion_halting {
+
+    public static void main(String[] args) {
+
+        int result = sum(5,10);
+        System.out.println(result);
+    }
+
+    public static int sum(int start,int end){
+
+        if(end > start){
+            return end + sum(start,end - 1);
+        }else{
+            return end;
+        }
+    }
+}
+
+//add all the numbers between 5~10 using recursion
