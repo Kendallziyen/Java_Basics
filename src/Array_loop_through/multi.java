6176ce41198dbe9fe65de152fcd0dff072a79d48
kendallziyen <huangzi435@gmail.com>
2024-01-20 13:14:28 +0800

    array

---
+++ src/Array_loop_through/multi.java
@@ -0,0 +1,24 @@
+package Array_loop_through;
+
+import java.util.Arrays;
+
+public class multi {
+
+    public static void main(String[] args){
+
+        int[][] arrayNum = {{0,1,2,3},{4,5,6}};
+
+        for (int[] arrayLoop: arrayNum){
+            System.out.println(Arrays.toString(arrayLoop));
+        }
+
+        System.out.println();
+
+        for (int[] arraycopy : arrayNum) {
+            for (int i : arraycopy) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
